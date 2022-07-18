3c93e51f3e0698ff1508152ecd02493fa10785c4
nurfawaiq <nurfawaiq@gmail.com>
2022-07-18 09:52:35 +0700

    update template package

--- yuknikah.sql
+++ yuknikah.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 23, 2022 at 03:20 AM
+-- Generation Time: Jul 18, 2022 at 02:52 AM
 -- Server version: 5.7.24
--- PHP Version: 7.4.12
+-- PHP Version: 8.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -60,7 +60,35 @@ INSERT INTO `contacts` (`id_contact`, `name_contact`, `name_alias`, `phone`, `em
 (13, 'Koko Bagus Gunarto', NULL, '(+62) 585 0898 3590', 'oyolanda@yahoo.co.id', 'Ds. Jamika No. 517, Banda Aceh 96176, Riau', NULL, 6, '2021-09-08 17:32:17', NULL, NULL),
 (14, 'Lili Haryanti', NULL, '0895 0915 1402', 'bhakim@yahoo.com', 'Jln. Suryo No. 406, Sorong 63322, Sumbar', NULL, 6, '2021-09-08 17:32:17', NULL, NULL),
 (15, 'Elisa Rahimah', '', '', 'prayogo72@gmail.co.id', 'Ds. Raden Saleh No. 957, Tebing Tinggi 53294, Pabar', '', 7, '2021-09-08 17:34:20', '2022-02-07 10:40:58', NULL),
-(16, 'Tukijo', '', '', '', '5 langkah dari rumah', '', 7, '2021-11-27 14:26:01', '2022-02-07 22:37:48', NULL);
+(16, 'Tukijo', '', '', '', '5 langkah dari rumah', '', 7, '2021-11-27 14:26:01', '2022-02-07 22:37:48', NULL),
+(17, 'Miss Gerry Oberbrunner', NULL, '1-267-559-4240', 'jamir.haley@ratke.net', '149 Vesta Route\nPort Roslyn, WI 57441-4989', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(18, 'Olga Hutagalung', NULL, '(+62) 658 6902 152', 'nadia.pradana@gmail.co.id', 'Ki. Suryo No. 750, Pontianak 20106, Kepri', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(19, 'Bancar Agus Mustofa', NULL, '0260 6484 5708', 'putu39@gmail.co.id', 'Dk. Basoka No. 227, Lubuklinggau 55592, Jambi', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(20, 'Bala Permadi', NULL, '0874 2694 257', 'melani.daru@yahoo.com', 'Psr. Lada No. 929, Semarang 71004, DIY', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(21, 'Timbul Langgeng Kuswoyo S.Farm', NULL, '(+62) 352 1034 4852', 'narpati.asmadi@gmail.com', 'Jr. Babakan No. 141, Ambon 12512, Sulteng', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(22, 'Maimunah Hasanah', NULL, '(+62) 817 356 291', 'lsaragih@gmail.co.id', 'Ds. Wahidin Sudirohusodo No. 757, Yogyakarta 56513, Lampung', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(23, 'Bakda Prayoga S.Psi', NULL, '0376 1122 4002', 'hakim.widya@yahoo.com', 'Psr. Bambu No. 47, Tasikmalaya 87560, Jambi', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(24, 'Tasnim Jarwadi Tamba M.Pd', NULL, '0870 0470 728', 'widiastuti.gara@yahoo.com', 'Psr. Rajawali No. 254, Probolinggo 36165, Jambi', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(25, 'Cindy Jamalia Suryatmi S.Farm', NULL, '(+62) 691 9730 0400', 'rrahayu@gmail.co.id', 'Gg. Raya Ujungberung No. 141, Manado 44667, Aceh', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(26, 'Tirta Gandi Irawan S.Pt', NULL, '(+62) 760 9271 0983', 'rahimah.belinda@yahoo.co.id', 'Psr. Abdul Muis No. 619, Administrasi Jakarta Timur 69423, Malut', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(27, 'Qori Susanti', NULL, '(+62) 607 0212 0064', 'safitri.farhunnisa@gmail.com', 'Ds. Baik No. 685, Administrasi Jakarta Pusat 61199, Jateng', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(28, 'Koko Bagus Gunarto', NULL, '(+62) 585 0898 3590', 'oyolanda@yahoo.co.id', 'Ds. Jamika No. 517, Banda Aceh 96176, Riau', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(29, 'Lili Haryanti', NULL, '0895 0915 1402', 'bhakim@yahoo.com', 'Jln. Suryo No. 406, Sorong 63322, Sumbar', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(30, 'Elisa Rahimah', NULL, NULL, 'prayogo72@gmail.co.id', 'Ds. Raden Saleh No. 957, Tebing Tinggi 53294, Pabar', NULL, 0, '2022-03-16 11:12:08', '2022-03-16 11:12:08', NULL),
+(31, 'Tukijo', '', '', '', '5 langkah dari rumah', '', 7, '2022-03-16 11:12:08', '2022-03-16 11:13:01', NULL),
+(32, 'Miss Gerry Oberbrunner', NULL, '1-267-559-4240', 'jamir.haley@ratke.net', '149 Vesta Route\nPort Roslyn, WI 57441-4989', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(33, 'Olga Hutagalung', NULL, '(+62) 658 6902 152', 'nadia.pradana@gmail.co.id', 'Ki. Suryo No. 750, Pontianak 20106, Kepri', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(34, 'Bancar Agus Mustofa', NULL, '0260 6484 5708', 'putu39@gmail.co.id', 'Dk. Basoka No. 227, Lubuklinggau 55592, Jambi', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(35, 'Bala Permadi', NULL, '0874 2694 257', 'melani.daru@yahoo.com', 'Psr. Lada No. 929, Semarang 71004, DIY', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(36, 'Timbul Langgeng Kuswoyo S.Farm', NULL, '(+62) 352 1034 4852', 'narpati.asmadi@gmail.com', 'Jr. Babakan No. 141, Ambon 12512, Sulteng', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(37, 'Maimunah Hasanah', NULL, '(+62) 817 356 291', 'lsaragih@gmail.co.id', 'Ds. Wahidin Sudirohusodo No. 757, Yogyakarta 56513, Lampung', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(38, 'Bakda Prayoga S.Psi', NULL, '0376 1122 4002', 'hakim.widya@yahoo.com', 'Psr. Bambu No. 47, Tasikmalaya 87560, Jambi', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(39, 'Tasnim Jarwadi Tamba M.Pd', NULL, '0870 0470 728', 'widiastuti.gara@yahoo.com', 'Psr. Rajawali No. 254, Probolinggo 36165, Jambi', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(40, 'Cindy Jamalia Suryatmi S.Farm', NULL, '(+62) 691 9730 0400', 'rrahayu@gmail.co.id', 'Gg. Raya Ujungberung No. 141, Manado 44667, Aceh', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(41, 'Tirta Gandi Irawan S.Pt', NULL, '(+62) 760 9271 0983', 'rahimah.belinda@yahoo.co.id', 'Psr. Abdul Muis No. 619, Administrasi Jakarta Timur 69423, Malut', NULL, 0, '2022-03-16 11:13:37', '2022-03-16 11:13:37', NULL),
+(42, 'Bancar Agus Mustofa', NULL, '0260 6484 5708', 'putu39@gmail.co.id', 'Dk. Basoka No. 227, Lubuklinggau 55592, Jambi', NULL, 0, '2022-03-16 11:13:57', '2022-03-16 11:13:57', NULL),
+(43, 'Bakda Prayoga S.Psi', NULL, '0376 1122 4002', 'hakim.widya@yahoo.com', 'Psr. Bambu No. 47, Tasikmalaya 87560, Jambi', NULL, 0, '2022-03-16 11:13:57', '2022-03-16 11:13:57', NULL),
+(44, 'Tasnim Jarwadi Tamba M.Pd', NULL, '0870 0470 728', 'widiastuti.gara@yahoo.com', 'Psr. Rajawali No. 254, Probolinggo 36165, Jambi', NULL, 0, '2022-03-16 11:13:57', '2022-03-16 11:13:57', NULL);
 
 -- --------------------------------------------------------
 
@@ -108,6 +136,7 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`id_group`, `name_group`, `info_group`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(0, 'Unrecognized', '', '2021-11-27 13:18:10', '2021-12-09 09:38:08', NULL),
 (1, 'Teman TK', NULL, NULL, NULL, NULL),
 (2, 'Teman SD', NULL, NULL, NULL, NULL),
 (3, 'Teman SMP', NULL, NULL, '2021-07-08 12:19:09', NULL),
@@ -116,8 +145,7 @@ INSERT INTO `groups` (`id_group`, `name_group`, `info_group`, `created_at`, `upd
 (6, 'Teman Kerja', 'PT. ABC', '2021-07-06 20:53:40', '2021-07-08 12:19:04', NULL),
 (7, 'Tetangga', '', '2021-08-01 14:10:47', '2021-08-01 14:10:59', NULL),
 (8, 'Saudara', '', '2021-08-01 14:11:03', '2021-08-01 14:11:03', NULL),
-(9, 'Mantan', '', '2021-08-01 14:11:07', '2021-08-01 14:11:10', '2021-08-01 14:11:10'),
-(10, 'New', '', '2021-11-27 13:18:10', '2021-12-09 09:38:08', '2021-12-09 09:38:08');
+(9, 'Mantan', '', '2021-08-01 14:11:07', '2021-08-01 14:11:10', '2021-08-01 14:11:10');
 
 -- --------------------------------------------------------
 
@@ -211,7 +239,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id_contact` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_contact` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `gawe`
@@ -223,7 +251,7 @@ ALTER TABLE `gawe`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id_group` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_group` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `migrations`
